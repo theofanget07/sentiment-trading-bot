03ad67aad9405e4e11a508c4b99e7ccf4e04d744
theofanget07 <theofanget07@gmail.com>
2026-02-04 08:24:21 +0100

    chore: create tests/ package for unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""Tests package - Unit and integration tests.
+
+This package contains all test files for the Sentiment Trading Bot.
+Tests should cover:
+- Portfolio management logic
+- API integrations (mocked)
+- Celery tasks
+- Bot commands
+"""
